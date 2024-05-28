ba7b47ed0be308e7f1f8604c6dbee4277310ba14
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-05-28 22:50:03 +0700

    add sheet when label onclik

--- app/src/main/java/com/nutriomatic/app/presentation/details/ProductDetailsActivity.kt
+++ app/src/main/java/com/nutriomatic/app/presentation/details/ProductDetailsActivity.kt
@@ -8,8 +8,14 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.nutriomatic.app.R
 import com.nutriomatic.app.databinding.ActivityProductDetailsBinding
+import com.nutriomatic.app.databinding.BottomSheetLayoutBinding
+import kotlin.random.Random
 
 class ProductDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailsBinding
@@ -28,6 +34,7 @@ class ProductDetailsActivity : AppCompatActivity() {
                 Toast.makeText(this, "Share Click", Toast.LENGTH_SHORT).show()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -48,7 +55,29 @@ class ProductDetailsActivity : AppCompatActivity() {
         with(binding) {
 //            Glide.with(this@ProductDetailsActivity)
 //                .load()
-            itemDescription.text = args.productId
+//            itemDescription.text = args.productId
+
+            Glide.with(this@ProductDetailsActivity)
+                .load("https://picsum.photos/seed/${Random.nextInt(1, 6)}/200/300")
+                .transform(CenterCrop(), RoundedCorners(20))
+                .into(itemImage)
+
+            imgLabel.setOnClickListener {
+                showBottomSheetDialog()
+            }
         }
+
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        val binding = BottomSheetLayoutBinding.inflate(layoutInflater)
+        bottomSheetDialog.setContentView(binding.root)
+
+        BottomSheetBehavior.from(binding.bottomSheetNutritionFact).apply {
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+        bottomSheetDialog.show()
     }
 }

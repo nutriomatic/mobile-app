3a22e0cfd91946e67e81e6e2455ecd979f3c6773
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-05-28 21:13:01 +0700

    add a title, back, and share in detail product

--- app/src/main/java/com/nutriomatic/app/presentation/details/ProductDetailsActivity.kt
+++ app/src/main/java/com/nutriomatic/app/presentation/details/ProductDetailsActivity.kt
@@ -1,6 +1,10 @@
 package com.nutriomatic.app.presentation.details
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.navArgs
 import com.bumptech.glide.Glide
@@ -11,10 +15,35 @@ class ProductDetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductDetailsBinding
     val args: ProductDetailsActivityArgs by navArgs()
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.product_detail_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_share -> {
+                // Lakukan tindakan ketika menu Share ditekan
+                Toast.makeText(this, "Share Click", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = "Detail Product"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         with(binding) {
 //            Glide.with(this@ProductDetailsActivity)

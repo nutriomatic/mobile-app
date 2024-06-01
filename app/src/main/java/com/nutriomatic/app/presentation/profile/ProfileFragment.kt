ed4d9955d37fa5484a9a781e25756c9d8d25bad0
aurelioklv <aurelioklv@gmail.com>
2024-06-01 17:31:04 +0700

    add datepicker and update text input

--- app/src/main/java/com/nutriomatic/app/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/nutriomatic/app/presentation/profile/ProfileFragment.kt
@@ -1,26 +1,18 @@
 package com.nutriomatic.app.presentation.profile
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.nutriomatic.app.R
 import com.nutriomatic.app.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
-    // Ekstensi fungsi untuk menampilkan Toast dengan mudah
-    fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
-        Toast.makeText(this, message, duration).show()
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,10 +26,18 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        with(binding){
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select birth date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+
+        with(binding) {
             txtBirthdayLayout.setEndIconOnClickListener {
-                // Respond to end icon presses
-                makeToast("Icon change birthday click")
+                datePicker.show(childFragmentManager, "date_picker")
+            }
+
+            datePicker.addOnPositiveButtonClickListener {
+                txtBirthdayInput.setText(datePicker.headerText)
             }
 
             txtBirthdayLayout.addOnEditTextAttachedListener {
@@ -57,13 +57,19 @@ class ProfileFragment : Fragment() {
                         makeToast("Edit clicked")
                         true
                     }
+
+                    R.id.menu_logout -> {
+                        makeToast("Logout clicked")
+                        true
+                    }
+
                     else -> false
                 }
             }
         }
     }
 
-    private fun makeToast(msg :String) {
+    private fun makeToast(msg: String) {
         Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
     }
 }
